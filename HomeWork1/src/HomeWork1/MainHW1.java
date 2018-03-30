1caa40a44dce4c0bc4899464eec7fbbaa4813a79
Danny Kadyshevitch <dankad@dannys-mbp.console-demo.adallom.com>
2018-03-30 09:11:22 +0300

    Last code changes - without printing last string to console + added new excel file

--- HomeWork1/src/HomeWork1/MainHW1.java
+++ HomeWork1/src/HomeWork1/MainHW1.java
@@ -12,58 +12,65 @@ import weka.core.Instance;
 import weka.core.Instances;
 
 public class MainHW1 {
-	
-	public static BufferedReader readDataFile(String filename) {
-		BufferedReader inputReader = null;
 
-		try {
-			inputReader = new BufferedReader(new FileReader(filename));
-		} catch (FileNotFoundException ex) {
-			System.err.println("File not found: " + filename);
-		}
+    public static BufferedReader readDataFile(String filename) {
+        BufferedReader inputReader = null;
 
-		return inputReader;
-	}
-		
-	/**
-	 * Sets the class index as the last attribute.
-	 * @param fileName
-	 * @return Instances data
-	 * @throws IOException
-	 */
-	public static Instances loadData(String fileName) throws IOException{
-		BufferedReader datafile = readDataFile(fileName);
+        try {
+            inputReader = new BufferedReader(new FileReader(filename));
+        } catch (FileNotFoundException ex) {
+            System.err.println("File not found: " + filename);
+        }
 
-		Instances data = new Instances(datafile);
-		data.setClassIndex(data.numAttributes() - 1);
-		return data;
-	}
-	
-	public static void main(String[] args) throws Exception {
-		
-		//load data
-    Instances data = loadData("wind_training.txt");
-				
-    LinearRegression test = new LinearRegression();
-	test.buildClassifier(data);	//find best alpha and build classifier with all attributes
+        return inputReader;
+    }
 
-	
-     Instances testData = loadData("wind_testing.txt");
-		
-		
-	LinearRegression test2 = new LinearRegression();
-	test2.buildClassifier(testData);
-	
-   		//build classifiers with all 3 attributes combinations
-	
-	
-	
-	//-------------------to erase befor submission ----------------------
-	//Instances dbugdata = loadData("wind_training -smallset.txt");
-	//LinearRegression test2 = new LinearRegression();
-	//test2.buildClassifier(dbugdata);
-    //test2.gradientDescent(dbugdata);
-		
-	}
+    /**
+     * Sets the class index as the last attribute.
+     *
+     * @param fileName
+     * @return Instances data
+     * @throws IOException
+     */
+    public static Instances loadData(String fileName) throws IOException {
+        BufferedReader datafile = readDataFile(fileName);
+
+        Instances data = new Instances(datafile);
+        data.setClassIndex(data.numAttributes() - 1);
+        return data;
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        //load training data
+        Instances data = loadData("wind_training.txt");
+
+        LinearRegression test = new LinearRegression();
+        test.buildClassifier(data);    //find best alpha and build classifier with all attributes
+
+        System.out.println("The chosen alpha is: " + test.getAlpha());
+        System.out.println("Training error with all features is: " + test.getError());
+
+        //load testing data
+        Instances testData = loadData("wind_testing.txt");
+
+        LinearRegression test2 = new LinearRegression();
+        test2.buildClassifier(testData);
+
+        System.out.println("Test error with all features is: " + test2.getError());
+
+        //build classifiers with all 3 attributes combinations
+        Instances trioData = loadData("wind_training.txt");
+
+        LinearRegression trainTrios = new LinearRegression();
+        trainTrios.findBestTrio(trioData, test.getAlpha());
+
+        //build classifiers with all 3 attributes combinations
+        Instances trioTestData = loadData("wind_testing.txt");
+
+        LinearRegression testTrios = new LinearRegression();
+        testTrios.findBestTrio(trioTestData, test.getAlpha());
+
+    }
 
 }
