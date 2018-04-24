3b9b89fa7205b94968afdf98fa39f9dc38721dc6
dankad <31929790+dankad@users.noreply.github.com>
2018-04-24 18:28:07 +0300

    complete feature scaler + updated KNN

--- HomeWork3/src/HomeWork3/FeatureScaler.java
+++ HomeWork3/src/HomeWork3/FeatureScaler.java
@@ -4,12 +4,27 @@ import weka.core.Instance;
 import weka.core.Instances;
 
 public class FeatureScaler {
-	/**
-	 * Returns a scaled version (using standarized normalization) of the given dataset.
-	 * @param instances The original dataset.
-	 * @return A scaled instances object.
-	 */
-	public Instances scaleData(Instances instances) {
-		return null;
-	}
+    /**
+     * Returns a scaled version (using standardized normalization) of the given dataset.
+     *
+     * @param instances The original dataset.
+     * @return A scaled instances object.
+     */
+    public Instances scaleData(Instances instances) {
+        Instances scaledInstances = instances;
+        int numOfInstances = scaledInstances.numInstances();
+        int numOfAttributes = scaledInstances.numAttributes();
+        double std_x, mean_x, scaledValue_x;
+        Instance currInstance;
+        for (int i = 0; i < numOfAttributes; i++) {
+            std_x = Math.pow(scaledInstances.variance(i), 2);
+            mean_x = scaledInstances.meanOrMode(i);
+            for (int j = 0; j < numOfInstances; j++) {
+                currInstance = scaledInstances.instance(j);
+                scaledValue_x = (currInstance.value(i) - mean_x) / std_x;
+                currInstance.setValue(i, scaledValue_x);
+            }
+        }
+        return scaledInstances;
+    }
 }
--- HomeWork3/src/HomeWork3/Knn.java
+++ HomeWork3/src/HomeWork3/Knn.java
@@ -4,37 +4,58 @@ import weka.classifiers.Classifier;
 import weka.core.Capabilities;
 import weka.core.Instance;
 import weka.core.Instances;
+import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.Remove;
 
 class DistanceCalculator {
     /**
-    * We leave it up to you wheter you want the distance method to get all relevant
-    * parameters(lp, efficient, etc..) or have it has a class variables.
-    */
-    public double distance (Instance one, Instance two) {
+     * We leave it up to you whether you want the distance method to get all relevant
+     * parameters(lp, efficient, etc..) or have it as a class variables.
+     */
+
+    private int m_PValue;
+
+    public double distance(Instance one, Instance two) {
         return 0.0;
     }
 
     /**
      * Returns the Lp distance between 2 instances.
+     *
      * @param one
      * @param two
      */
     private double lpDistance(Instance one, Instance two) {
-        return 0.0;
+        double[] oneValues = one.toDoubleArray();
+        double[] twoValues = two.toDoubleArray();
+        double sumLp = 0;
+        for (int i = 0; i < one.numAttributes(); i++) {
+            sumLp += Math.pow((oneValues[i] - twoValues[i]), m_PValue);
+        }
+        return Math.pow(sumLp, 1 / m_PValue);
     }
 
     /**
      * Returns the L infinity distance between 2 instances.
+     *
      * @param one
      * @param two
      * @return
      */
     private double lInfinityDistance(Instance one, Instance two) {
-        return 0.0;
+        double[] oneValues = one.toDoubleArray();
+        double[] twoValues = two.toDoubleArray();
+        double tempDiff, max = Integer.MIN_VALUE;
+        for (int i = 0; i < one.numAttributes(); i++) {
+            tempDiff = Math.abs(oneValues[i] - twoValues[i]);
+            max = tempDiff > max ? tempDiff : max;
+        }
+        return max;
     }
 
     /**
      * Returns the Lp distance between 2 instances, while using an efficient distance check.
+     *
      * @param one
      * @param two
      * @return
@@ -45,6 +66,7 @@ class DistanceCalculator {
 
     /**
      * Returns the Lp distance between 2 instances, while using an efficient distance check.
+     *
      * @param one
      * @param two
      * @return
@@ -56,7 +78,10 @@ class DistanceCalculator {
 
 public class Knn implements Classifier {
 
-    public enum DistanceCheck{Regular, Efficient}
+    public enum DistanceCheck {Regular, Efficient}
+
+    public int m_k;
+
     private Instances m_trainingInstances;
 
     @Override
@@ -66,11 +91,11 @@ public class Knn implements Classifier {
      * @param instances
      */
     public void buildClassifier(Instances instances) throws Exception {
-
     }
 
     /**
      * Returns the knn prediction on the given instance.
+     *
      * @param instance
      * @return The instance predicted value.
      */
@@ -82,44 +107,81 @@ public class Knn implements Classifier {
      * Caclcualtes the average error on a give set of instances.
      * The average error is the average absolute error between the target value and the predicted
      * value across all insatnces.
+     *
      * @param insatnces
      * @return
      */
-    public double calcAvgError (Instances insatnces){
+    public double calcAvgError(Instances insatnces) {
         return 0.0;
     }
 
     /**
      * Calculates the cross validation error, the average error on all folds.
-     * @param insances Insances used for the cross validation
+     *
+     * @param insances     Insances used for the cross validation
      * @param num_of_folds The number of folds to use.
      * @return The cross validation error.
      */
-    public double crossValidationError(Instances insances, int num_of_folds){
+    public double crossValidationError(Instances insances, int num_of_folds) {
         return 0.0;
     }
 
 
     /**
      * Finds the k nearest neighbors.
+     *
      * @param instance
      */
-    public /* Collection of your choice */ findNearestNeighbors(Instance instance) {
+    public int[] findNearestNeighbors(Instance instance) {
+        double[][] distForNeighbors = new double[m_trainingInstances.size()][2];
+        double tempDist;
+        int[] nearestNeighbors = new int[m_k];
 
+        // calculate the distance of @instance from all instance in the data set
+        for (int i = 0; i < m_trainingInstances.size(); i++) {
+            tempDist = lpDistance(instance, m_trainingInstances.instance(i));
+            if (tempDist != 0) {
+                distForNeighbors[i][0] = i;
+                distForNeighbors[i][1] = tempDist;
+            } else {
+                distForNeighbors[i][0] = i;
+                distForNeighbors[i][1] = Integer.MAX_VALUE;
+            }
+        }
+
+        // sort indexes by dist from @instance
+        int n = m_trainingInstances.size();
+        for (int i = 0; i < n; i++) {
+            double[] tempNeighbor = distForNeighbors[i];
+            int j = i - 1;
+            while (j >= 0 && distForNeighbors[j][1] > tempNeighbor[1]) {
+                distForNeighbors[j + 1] = distForNeighbors[j];
+                j = j - 1;
+            }
+            distForNeighbors[j + 1] = tempNeighbor;
+        }
+
+        // set the array with the indexes of nearest neighbors
+        for (int i = 0; i < m_k; i++) {
+            nearestNeighbors[i] = (int) distForNeighbors[i][0];
+        }
+        return nearestNeighbors;
     }
 
     /**
      * Cacluates the average value of the given elements in the collection.
+     *
      * @param
      * @return
      */
-    public double getAverageValue (/* Collection of your choice */) {
+    public double getAverageValue(/* Collection of your choice */) {
         return 0.0;
     }
 
     /**
      * Calculates the weighted average of the target values of all the elements in the collection
      * with respect to their distance from a specific instance.
+     *
      * @return
      */
     public double getWeightedAverageValue(/* Collection of your choice */) {
